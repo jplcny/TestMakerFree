330cd314e3a425a078f8d83f4ee7cc86bdb1915b
jlafountaindnw <j.lafountain@dwdiesel.com>
2018-04-19 19:23:05 -0400

    Add project files.

---
+++ TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, Inject } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+    selector: "quiz",
+    templateUrl: './quiz.component.html',
+    styleUrls: ['./quiz.component.css']
+})
+
+export class QuizComponent {
+    quiz: Quiz;
+
+    constructor(private activateRoute: ActivatedRoute,
+        private router: Router,
+        private http: HttpClient) {
+
+        // create an empty object from the Quiz interface
+        this.quiz = <Quiz>{};
+
+        var id = +this.activateRoute.snapshot.params["id"];
+        console.log(id);
+        if (id) {
+            // TODO: load the quiz using server-side API
+        }
+        else {
+            console.log("Invalid id: routing back to home...");
+            this.router.navigate(["home"]);
+        }
+    }
+}
